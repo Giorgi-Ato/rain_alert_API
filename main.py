f4fc7638b5514044b30947f07c2df386f2fbe2f6
Giorgi Seperidze <111276780+Giorgi-Ato@users.noreply.github.com>
2022-09-20 23:00:33 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from twilio.rest import Client
+import datetime
+from data import compliments
+
+
+END_POINT = "https://api.openweathermap.org/data/2.5/weather"
+api_key = "a86058055fc6f6c0949ebed75cbe3c8a"
+lat = 41.616756
+lon = 41.636745
+
+account_sid = "AC82c6254fb51a807858cbe12be77bdf02"
+auth_token = "3e51ef26d5d54336ecba54aa8127e861"
+
+
+parameters = {
+    "lat": lat,
+    "lon": lon,
+    "appid": api_key,
+    "lang": "ua",
+    "units": "metric",
+}
+
+response = requests.get(url=END_POINT, params=parameters)
+response.raise_for_status()
+data = response.json()
+
+weather = data['weather'][0]['description']
+temp = data['main']['temp']
+
+sms = f"Сьогодні очікується {weather}. температура повітря буде {temp} градусів. " \
+      + compliments[datetime.datetime.now().day]
+
+client = Client(account_sid, auth_token)
+message = client.messages \
+            .create(
+                 body=sms,
+                 from_="My Num",
+                 to="Receiver's Num",
+             )
